31820ccb3b392a4114d7f2b717d3748b1c853b3d
Daniel Schmitt <3575239+f1nan@users.noreply.github.com>
2019-04-18 19:00:35 +0200

    Update script for server configuration

--- server_config.sh
+++ server_config.sh
@@ -2,17 +2,24 @@
 #
 # Beispiel-Script zur Konfiguration einer Hostmaschine fuer den
 # KOCHA-Server
-#
 
 HOST=$(hostname)
 PORT=9999
 PYTHON3=$(which python3)
+CMD="$PYTHON3 -m kocha.server $HOST $PORT"
 
 # Eingehende Anfragen ueber TCP/IP auf Port erlauben
-sudo ufw allow proto tcp from any to any port $PORT
+sudo ufw allow proto tcp from any to any port "$PORT"
 
 # Um die Regel wieder aufzuheben, nachstehenden Befehl ausfuehren
-#sudo ufw delet allow proto tcp from any to any port $PORT
+#sudo ufw delete allow proto tcp from any to any port $PORT
 
-# cron-job einrichten, der den KOCHA-Server beim Booten startet
-sudo crontab -l 2>/dev/null | echo "$PYTHON3 kocha.server $HOST $PORT" | crontab -
+# Falls noch nicht vorhanden, cron-job einrichten, der den KOCHA-Server
+# beim Booten startet
+sudo crontab -l 2>/dev/null | grep "@reboot $CMD" >/dev/null
+if [[ $? == 1 ]]; then
+    sudo crontab -l 2>/dev/null; echo "@reboot $CMD" | sudo crontab -
+fi
+
+# KOCHA-Server jetzt starten
+$CMD 2>/dev/null &
